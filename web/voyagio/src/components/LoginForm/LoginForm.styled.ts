f165764b76911061bf38f9e88728d9aeaa573ea9
slry <v.rybalchenko@innopolis.university>
2023-06-22 21:22:32 +0300

    feat: add login form

---
+++ web/voyagio/src/components/LoginForm/LoginForm.styled.ts
@@ -0,0 +1,35 @@
+import styled from '@emotion/styled';
+import { Button, ButtonProps, createPolymorphicComponent } from '@mantine/core';
+
+export const LoginFormContainer = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  min-width: 304px;
+`;
+
+export const LoginInputContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 32px;
+`;
+
+export const Icon = styled.img`
+  size: 24px;
+`;
+
+const _GoogleButton = styled(Button)`
+  box-shadow: 0px 5px 12px 0px rgba(130, 130, 130, 0.1);
+
+  & .mantine-Button-label {
+    gap: 10px;
+  }
+
+  &:hover {
+    background-color: rgba(193, 194, 197, 0.45);
+  }
+`;
+
+export const GoogleButton = createPolymorphicComponent<'button', ButtonProps>(
+  _GoogleButton
+);

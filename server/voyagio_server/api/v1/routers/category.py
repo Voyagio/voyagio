35c70f1875a8b8d9be946a772a7f76fae6fb9ec0
Egor Kuziakov <egor.kuziakov@gmail.com>
2023-06-25 16:34:38 +0300

    Small changes in category and place routers

--- server/voyagio_server/api/v1/routers/category.py
+++ server/voyagio_server/api/v1/routers/category.py
@@ -5,14 +5,14 @@ from api.v1.schemas import category as category_schemas
 from crud import category as category_crud
 from database import get_session
 
-category_router = APIRouter(prefix="", tags=["category"])
+category_router = APIRouter(prefix="/categories", tags=["category"])
 
 
-@category_router.post("/categories", response_model=category_schemas.Category)
+@category_router.post("", response_model=category_schemas.Category)
 async def create_category(category: category_schemas.CategoryCreate, db: Session = Depends(get_session)):
     return category_crud.create_category(session=db, category=category)
 
 
-@category_router.get("/categories", response_model=list[category_schemas.Category])
+@category_router.get("", response_model=list[category_schemas.Category])
 async def get_categories(offset: int = 0, limit: int = 100, db: Session = Depends(get_session)):
     return category_crud.get_categories(session=db, offset=offset, limit=limit)

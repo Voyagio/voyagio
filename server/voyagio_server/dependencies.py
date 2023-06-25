478354c0c4e092568eb78a01f81172fb67294cff
Egor Kuziakov <egor.kuziakov@gmail.com>
2023-06-25 16:18:07 +0300

    Add auth dependency

--- server/voyagio_server/dependencies.py
+++ server/voyagio_server/dependencies.py
@@ -1,10 +1,21 @@
-from fastapi.security import HTTPBearer
-
 import uuid
 
-from models import user as user_models
+import jwt
+from fastapi import Depends, HTTPException
+from fastapi.security import HTTPBearer
+
+security = HTTPBearer()
 
 
-def get_current_user() -> user_models.User:
-    user_id = uuid.UUID("b0322811-b1fd-420e-b8d3-afdc31cc7ec7")
-    return user_models.User(user_id=user_id, email="mail@example.org")
+def get_user_id_from_token(access_token: str) -> uuid.UUID | None:
+    payload = jwt.decode(access_token, key="secret", algorithms=["HS256"])
+    if "user_id" not in payload:
+        return None
+    return uuid.UUID(payload["user_id"])
+
+
+def get_current_user_id(auth=Depends(security)) -> uuid.UUID:
+    user_id = get_user_id_from_token(auth.credentials)
+    if not user_id:
+        raise HTTPException(status_code=401, detail="Invalid access token")
+    return user_id

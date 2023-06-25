afa9dbd47826e7378b167d6a66943dd12d0c540d
Egor Kuziakov <egor.kuziakov@gmail.com>
2023-06-25 14:41:30 +0300

    Add collection model

---
+++ server/voyagio_server/models/collection.py
@@ -0,0 +1,19 @@
+import uuid
+
+from sqlalchemy import Table, Column, String, ForeignKey
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import Relationship
+
+from database import Base
+
+CollectionPlace = Table('collection_place', Base.metadata,
+                        Column('collection_id', UUID, ForeignKey('collections.id')),
+                        Column('place_id', UUID, ForeignKey('places.id')))
+
+
+class Collection(Base):
+    __tablename__ = "collections"
+    id = Column(UUID(as_uuid=True), primary_key=True, index=True, default=uuid.uuid4)
+    name = Column(String, nullable=False)
+    author_id = Column(UUID(as_uuid=True), ForeignKey("users.id"))
+    places = Relationship('Place', secondary='collection_place')

f7eb371a5c2be3cc53aaaf5971d4e54d9a970db0
Egor Kuziakov <egor.kuziakov@gmail.com>
2023-06-28 18:48:09 +0300

    fix: get_current_user changes from header to cookie

--- server/voyagio_server/dependencies.py
+++ server/voyagio_server/dependencies.py
@@ -2,6 +2,7 @@ import uuid
 
 import jwt
 from fastapi import Depends, HTTPException
+from fastapi import Request
 from fastapi.security import HTTPBearer
 from sqlalchemy.orm import Session
 
@@ -21,12 +22,14 @@ def get_user_id_from_token(access_token: str) -> uuid.UUID | None:
     return uuid.UUID(payload["user_id"])
 
 
-def get_current_user_id(auth=Depends(security)) -> uuid.UUID:
-    user_id = get_user_id_from_token(auth.credentials)
+def get_current_user_id(request: Request) -> uuid.UUID:
+    access_token = request.cookies.get("access_token")
+    user_id = get_user_id_from_token(access_token)
     if not user_id:
         raise HTTPException(status_code=401, detail="Invalid access token")
     return user_id
 
 
-def get_current_user(db: Session = Depends(get_session), user_id: uuid.UUID = Depends(get_current_user_id)) -> User | None:
+def get_current_user(db: Session = Depends(get_session),
+                     user_id: uuid.UUID = Depends(get_current_user_id)) -> User | None:
     return db.query(User).filter(User.id == user_id).one_or_none()

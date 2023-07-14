9f3ccc34f9f717c9cd5ec1e86adec324e8ba297b
slry <v.rybalchenko@innopolis.university>
2023-07-14 21:21:14 +0300

    style: fix img height

--- web/voyagio/src/components/AttractionCard/AttractionCard.styled.ts
+++ web/voyagio/src/components/AttractionCard/AttractionCard.styled.ts
@@ -2,46 +2,49 @@ import styled from '@emotion/styled';
 
 export const CardContainer = styled.div`
   height: 100%;
-  
+
   display: flex;
   flex-direction: column;
   gap: 16px;
-  
+
   padding-bottom: 16px;
   padding-left: 16px;
   padding-right: 16px;
-  
+
   overflow: hidden;
-  
+
   border-radius: 16px;
   background-color: white;
 
-  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.05), 0 36px 28px -7px rgba(0, 0, 0, 0.05), 0 17px 17px -7px rgba(0, 0, 0, 0.04);
-  
+  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.05),
+    0 36px 28px -7px rgba(0, 0, 0, 0.05), 0 17px 17px -7px rgba(0, 0, 0, 0.04);
+
   & > div {
     height: 100%;
   }
 `;
 
 export const AttractionImage = styled.img`
+  min-height: 271px;
   height: 271px;
+  width: 339px;
   margin-left: -16px;
   margin-right: -16px;
-  
+
   object-fit: cover;
 `;
 
 export const Address = styled.p`
-  color: #ADB5BD;
+  color: #adb5bd;
   font-weight: 300;
 `;
 
 export const AdditionalInfoContainer = styled.div`
   display: flex;
   justify-content: space-between;
-  
+
   font-weight: bold;
-  
+
   margin-top: 12px;
 `;
 
@@ -49,6 +52,4 @@ export const ButtonsContainer = styled.div`
   margin-top: 20px;
   display: flex;
   gap: 12px;
-  
-  ;
 `;
